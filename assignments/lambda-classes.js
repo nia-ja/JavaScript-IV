1429ab2de6b1eff6b3a1a6be85abb4c02e86e66f
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-01-24 07:23:09 -0800

    Added sub-classes Instructor and Student, created 2 Insructor Objects and 2 Student Objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -15,3 +15,135 @@ class Person {
 } //<-----Person
 
 
+
+//subClass Instructor
+class Instructor extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+
+    demo(subject) {
+        return `Today we are learning about ${subject}.`;
+    }
+
+    grade(student, subject) {
+        this.name = student.name;
+        return `${student.name} receives a perfect score on ${subject}.`;
+    }
+} // <------ Instructor
+
+
+
+
+//subClass Student
+class Student extends Person {
+    constructor(attributes) {
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+
+    listsSubjects() {
+        // const subjects = this.favSubjects;
+        // function everySubject(arr, cb) {
+        //     let length = arr.length;
+        //     for (let i = 0; i < length; i++) {
+        //         cb(arr[i]);
+        //     }
+        // }
+
+        // everySubject(subjects, function(value){
+        //     console.log(value);
+        // });
+
+        const subjects = this.favSubjects;
+        subjects.forEach(function(subject) {
+            console.log(subject);
+        });
+    }
+
+    PRAssignments(subject) {
+        return `${this.name} has submitted a PR for ${subject}.`;
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`;
+    } 
+} // <------ Student
+
+
+
+class ProjectManager extends Instructor {
+    constructor(attributes) {
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+
+    standUp() {
+
+    }
+
+    debugsCode() {
+
+    }
+} // <------ ProjectManager
+
+//Instructor objects
+const mario = new Instructor({
+    name: "Mario",
+    age: 37,
+    location: "Mushroom Kingdom",
+    gender: "male",
+    specialty: "Front-end",
+    favLanguage: "CSS",
+    catchPhrase: "I'm an Italian plumber"
+});
+
+const tania = new Instructor({
+    name: "Tania",
+    age: 18,
+    location: "Livermore, CA",
+    gender: "female",
+    specialty: "Computer Sience",
+    favLanguage: "Python",
+    catchPhrase: "print('Hello, world!')"
+});
+
+//Student objects
+
+const mike = new Student({
+    name: "Mike",
+    age: "30",
+    location: "Ohio",
+    gender: "male",
+    previousBackground: "gender studies",
+    className: "Web15",
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
+});
+
+const lina = new Student({
+    name: "Lina",
+    age: "25",
+    location: "Charlotte, NC",
+    gender: "female",
+    previousBackground: "biology",
+    className: "Webpt4",
+    favSubjects: ['CSS', 'JavaScript', 'Python']
+});
+
+console.log(mario.speak());
+console.log(mario.specialty);
+console.log(mario.demo('HTML'));
+console.log(tania.catchPhrase);
+console.log(tania.speak());
+console.log(mike.speak());
+console.log(mike.favSubjects);
+console.log(mike.listsSubjects());
+console.log(tania.grade(mike, 'CSS'));
+console.log(mike.PRAssignments('"JS IV"'));
+console.log(mike.sprintChallenge('"CSS"'));
